f08b3da87f86f8c3f34da94c89858d630d56b0f7
MuYunLiQiu <1603728172@qq.com>
2025-08-20 04:08:42 +0800

    初始登录页面完成部分

---
+++ src/pages/404.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <el-result
+        icon="warning"
+        title="404提示"
+         sub-title="没这个页面啊笨比，你再试试"
+        >
+        <template #extra>
+        <el-button type="primary" @click="$router.push('/')">返回首页</el-button>
+        </template>
+      </el-result>
+    </div>
+</template>
