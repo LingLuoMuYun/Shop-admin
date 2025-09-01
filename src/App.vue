adc14e28ade09cb1824957d08f9c483331c2732b
MuYunLiQiu <1603728172@qq.com>
2025-09-01 23:05:53 +0800

    统计面板组件开发——骨架优化体验

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,12 @@
   <router-view></router-view>
 </template>
 
-<style scoped>
+<style >
+
+  body{
+    @apply bg-gray-100;
+  }
+
   #nprogress .bar{
     background-color: #f4f4f4!important;
     height: 3px!important;
