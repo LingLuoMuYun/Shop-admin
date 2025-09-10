95cd00eae256335652abd2ab3d573bebf5c92377
MuYunLiQiu <1603728172@qq.com>
2025-09-11 01:58:59 +0800

    图片列表组件开发

--- src/components/imageMain.vue
+++ src/components/imageMain.vue
@@ -1,7 +1,20 @@
 <template>
   <el-main class="image-main" v-loading="loading">
-    <div class="top">
-      <div v-for="(item,index) in list" :key="index">{{ item.url }}</div>
+    <div class="top p-3">
+       <el-row :gutter="10">
+        <el-col v-for="(item,index) in list" :key="index" :span="6" :offset="0">
+          <el-card shadow="hover" class="relative mb-3" :body-style="{ 'padding':0 }">
+              <el-image :src="item.url" fit="cover" :lazy="true" class="w-full h-[150px]" style="width:100%"></el-image>
+              <div class="image-title">{{ item.name }}</div>
+            <div class="flex items-center justify-center p-2">
+              <el-button type="primary" size="small" text>重命名</el-button>
+              <el-button type="primary" size="small" text>删除</el-button>
+            </div>
+          </el-card>
+          
+        </el-col>
+       </el-row>
+       
     </div>
     <div class="bottom">
       <el-pagination background layout="prev,pager, next" :total="total" :current-page="currentPage" :page-size="limit" @current-change="getData" />
@@ -70,5 +83,12 @@
   left: 0;
   right: 0;
   @apply flex items-center justify-center;
+}
+.image-title{
+  position:absolute;
+  top: 122px;
+  left: -1px;
+  right: -1px;
+  @apply text-sm truncate text-gray-100 bg-opacity-50 bg-gray-700 px-2 py-1;
 }
 </style>
