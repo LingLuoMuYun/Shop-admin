1f8e61b8eac2898fa84231b93c358bcba4a4ee06
MuYunLiQiu <1603728172@qq.com>
2025-09-24 18:19:41 +0800

    商品模块——复用代码实现

--- src/composables/util.js
+++ src/composables/util.js
@@ -1,5 +1,7 @@
 import { ElNotification,ElMessageBox } from 'element-plus'
 import nprogress from 'nprogress'
+
+
 export function toast(message,type="success",dangerouslyUserHTMLString=true){
     ElNotification({
         message,
@@ -28,6 +30,19 @@ export function showPrompt(tip,value = ""){
     cancelButtonText: '取消',
     inputValue:value
   })
+}
+
+//将query对象转成url参数
+export function queryParams(query){
+  let q = []
+  for(const key in query){
+      if(query[key]){
+          q.push(`${key}=${encodeURIComponent(query[key])}`)
+      }
+  }
+  let r = q.join("&")
+  r= r ? ("?"+r) :""
+  return r
 }
 
 //显示全屏loading
