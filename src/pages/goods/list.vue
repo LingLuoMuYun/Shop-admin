44dff9e63c9b6b10fcb1a882051aace8e8bcb1b5
MuYunLiQiu <1603728172@qq.com>
2025-09-26 15:47:09 +0800

    商品模块——高级搜索功能实现

--- src/pages/goods/list.vue
+++ src/pages/goods/list.vue
@@ -12,10 +12,28 @@
                             <el-input v-model="searchForm.title" placeholder="商品名称" clearable></el-input>
                         </el-form-item>
                     </el-col>
-                    <el-col :span="8" :offset="8">
+                    <el-col :span="8" :offset="0" v-if="showSearch">
+                        <el-form-item label="商品分类" prop="category_id">
+                            <el-select v-model="searchForm.category_id" placeholder="请选择商品分类" clearable>
+                                <el-option v-for="item in category_list"
+                                    :key="item.id"
+                                    :label="item.name"
+                                    :value="item.id">
+                                </el-option>
+                            </el-select>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="8" :offset="showSearch ? 0 : 8">
                         <div class="flex items-center justify-end">
                             <el-button type="primary" @click="getData">搜索</el-button>
                             <el-button @click="resetSearchForm">重置</el-button>
+                            <el-button type="primary" text @click="showSearch = !showSearch">
+                                {{ showSearch ? '收起' : '展开' }}
+                                <el-icon>
+                                    <ArrowUp v-if="showSearch"/>
+                                    <ArrowDown v-else/>
+                                </el-icon>
+                            </el-button>
                         </div>
                     </el-col>
                 </el-row>
@@ -126,6 +144,11 @@ import ListHeader from "~/components/ListHeader.vue"
 import FormDrawer from "~/components/FormDrawer.vue"
 import ChooseImage from "~/components/ChooseImage.vue"
 import { useInitTable,useInitForm } from "~/composables/useCommon" 
+import {
+    getCategoryList
+} from "~/api/category"
+
+
 const roles = ref([]);
 
 const {
@@ -201,4 +224,10 @@ const tabbars = [{
     key:"delete",
     name:"回收站"
 },]
+
+//商品分类
+const category_list = ref([])
+getCategoryList().then(res=>category_list.value = res)
+
+const showSearch = ref(false)
 </script>
