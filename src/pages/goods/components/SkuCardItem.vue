7986108d31426e5dc0f555241d19707fd65cf2b7
MuYunLiQiu <1603728172@qq.com>
2025-10-11 03:17:08 +0800

    商品模块——多SKU规格列表——渲染选项的值

--- src/pages/goods/components/SkuCardItem.vue
+++ src/pages/goods/components/SkuCardItem.vue
@@ -1,10 +1,32 @@
 <template>
-    <div v-for="(tag,index) in item.goodsSkusCardValue" :key="index">
-        {{ tag.text }}
-    </div>
+    <div class="flex gap-2">
+    <el-tag
+      v-for="(tag,index) in item.goodsSkusCardValue"
+      :key="index"
+      closable
+      :disable-transitions="false"
+      @close="handleClose(tag)"
+      effect="plain"
+    >
+      {{ tag.text }}
+    </el-tag>
+    <el-input
+      v-if="inputVisible"
+      ref="InputRef"
+      v-model="inputValue"
+      class="w-20"
+      size="small"
+      @keyup.enter="handleInputConfirm"
+      @blur="handleInputConfirm"
+    />
+    <el-button v-else class="button-new-tag" size="small" @click="showInput">
+      + New Tag
+    </el-button>
+  </div>
+
 </template>
 
-<script setup>
+<script setup> 
 import {
     initSkusCardItem
 }from "~/composables/useSku.js"
@@ -14,6 +36,13 @@ const props = defineProps({
 })
 
 const {
-    item
+    item,
+    inputValue,
+    inputVisible,
+    InputRef,
+    handleClose,
+    showInput,
+    handleInputConfirm
 } = initSkusCardItem(props.skuCardId)
+
 </script>
