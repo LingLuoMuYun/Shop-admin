e7721eb28b6851c06b9778fb611e80204cbf76f5
MuYunLiQiu <1603728172@qq.com>
2025-10-22 23:37:02 +0800

    订单模块——查看物流信息功能实现以及同意拒绝退款

--- src/api/order.js
+++ src/api/order.js
@@ -1,5 +1,5 @@
 import axios from '~/axios'
-import { queryParams } from '~/composables/util' 
+import { queryParams } from "~/composables/util"
 
 export function getOrderList(page,query = {}){
     let r = queryParams(query)
@@ -18,4 +18,12 @@ export function exportOrder(query = {}){
     return axios.post(`/admin/order/excelexport${r}`,{},{
         responseType:"blob"
     })
+}
+
+export function getShipInfo(id){
+    return axios.get(`/admin/order/${id}/get_ship_info`)
+}
+
+export function refundOrder(id,data){
+    return axios.post(`/admin/order/${id}/handle_refund`,data)
 }
--- src/pages/order/ExportExcel.vue
+++ src/pages/order/ExportExcel.vue
@@ -1,6 +1,6 @@
 <template>
     <el-drawer title="导出订单" v-model="dialogVisible" size="40%">
-        <el-form :model="form"  label-width="80px" >
+        <el-form :model="form" label-width="80px">
             <el-form-item label="订单类型">
                 <el-select v-model="form.tab" placeholder="请选择">
                     <el-option v-for="item in tabs"
@@ -9,31 +9,37 @@
                         :value="item.key">
                     </el-option>
                 </el-select>
-                
             </el-form-item>
             <el-form-item label="时间范围">
-                <el-date-picker v-model="form.time" type="daterange" value-format="YYYY-MM-DD"
-                range-separator="至" start-placeholder="开始日期" end-placeholder="结束日期"/>
+                <el-date-picker
+                    v-model="form.time"
+                    type="daterange"
+                    range-separator="至"
+                    start-placeholder="开始日期"
+                    end-placeholder="结束日期"
+                    value-format="YYYY-MM-DD"
+                />
             </el-form-item>
             <el-form-item>
                 <el-button type="primary" @click="onSubmit" :loading="loading">导出</el-button>
             </el-form-item>
         </el-form>
     </el-drawer>
-    
 </template>
-
 <script setup>
 import { ref,reactive } from "vue"
-import { exportOrder } from "~/api/order"
-import { toast } from "~/composables/util"
+import {
+    exportOrder
+} from "~/api/order"
+import { toast } from "~/composables/util";
 defineProps({
     tabs:Array
 })
-
 const dialogVisible = ref(false)
+
 const open = ()=>dialogVisible.value = true
 const close = ()=>dialogVisible.value = false
+
 const form = reactive({
     tab:null,
     time:""
@@ -41,7 +47,7 @@ const form = reactive({
 
 const loading = ref(false)
 const onSubmit = ()=>{
-    if(!form.tab) return toDarkStyle("订单类型不能为空","error")
+    if(!form.tab) return toast("订单类型不能为空","error")
     loading.value = true
     let starttime = null
     let endtime = null
@@ -58,7 +64,7 @@ const onSubmit = ()=>{
         let link = document.createElement("a")
         link.style.display = "none"
         link.href = url
-        let filename = (new Date()).getTime()+".xlsx"
+        let filename = (new Date()).getTime() + ".xlsx"
         link.setAttribute("download",filename)
         document.body.appendChild(link)
         link.click()
